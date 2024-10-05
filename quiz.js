c81dbaf3d3f945896e0d873a405a0a43946e95d8
Dennis Otieno <dennisotieno.dev@gmail.com>
2024-10-06 01:00:37 +0300

    Changes

--- quiz.js
+++ quiz.js
@@ -3,7 +3,7 @@ function checkAnswer () {
     let feedback = document.getElementById( "feedback" )
     let userAnswer = document.querySelector( '[name="quiz"]:checked' ).value;
     if (userAnswer === correctAnswer) {
-        feedback.textContent = "Correct, Well done.";
+        feedback.textContent = "Correct! Well done.";
     } else {
         feedback.textContent = "That's incorrect. Try again!";
     }
